099407253dcb702edeafb726d20a7b5e5eb3ac67
blackbeltninja8 <56656746+blackbeltninja8@users.noreply.github.com>
2019-10-23 14:46:50 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -13,7 +13,8 @@ preload: function()
 	
 	this.load.spritesheet('idle', 'SpriteSheets/idle.png', {frameWidth: 210, frameHeight: 375});
 	this.load.spritesheet('basicAttack', 'SpriteSheets/LPLKHPHK.png', {frameWidth: 210 , frameHeight: 375});
-	this.load.spritesheet('crouch','SpriteSheets/crounchAttaks.png', {frameWidth: 210, frameHeight: 200});
+	this.load.spritesheet('crouch','SpriteSheets/crounchAttaks.png', {frameWidth: 210, frameHeight: 190});
+	this.load.spritesheet('crouchBig','SpriteSheets/crounchAttaksBig.png', {frameWidth: 306, frameHeight: 250});
 	this.load.spritesheet('fallSpecial','SpriteSheets/fallSpecial.png', {frameWidth: 210, frameHeight: 375});
 	this.load.spritesheet('lPunch', 'SpriteSheets/lightPunchRe.png', {frameWidth: 210, frameHeight: 375}); //100 by 31 hitbox, roughly 100 pixels down from top 
 	this.load.spritesheet('trans', 'SpriteSheets/transparent.png', {frameWidth: 210, frameHeight: 375}); //100 by 31 hitbox, roughly 100 pixels down from top 
@@ -63,6 +64,12 @@ create: function()
 		frameRate: 6,
 		repeat: false
 	});
+	this.anims.create({
+		key: 'crounch',
+		frames: this.anims.generateFrameNumbers('crouchBig', {frames: [0, 1]}),
+		frameRate: 6,
+		repeat: false
+	});
 	/*var forwardDash = this.input.keyboard.createCombo(['39', '39'] {
 		resetOnWrongKey: true, 
 		maxKeyDelay: 1000, 
@@ -77,6 +84,7 @@ create: function()
 		this.player.anims.play('lP');
 	}*/
 	hit = 0;
+	jump = 0;
 },
 
 
@@ -85,18 +93,25 @@ update: function (time, delta)
 
 	this.player.body.setVelocity(0);
 	this.player.anims.play('idleAnim', true);
-	if (this.cursors.left.isDown) {
+	if (this.cursors.left.isDown && this.cursors.down.isUp) {
 		this.player.body.setVelocityX(-80);
 	}
-	else if (this.cursors.right.isDown) {
+	else if (this.cursors.right.isDown && this.cursors.down.isUp) {
 		this.player.body.setVelocityX(80);
 	}
-	if (this.cursors.up.isDown && this.player.body.touching.down) {
+	if (this.cursors.up.isDown && /*this.player.body.touching.down &&*/ jump <= 0) {
 		this.player.body.setVelocityY(-6500);
+		jump += 1;
 	}
-	else if (this.cursors.down.isDown) {
+	if (this.player.body.touching.down) {
+		jump = 0;
+	}
+	if (this.cursors.down.isDown) {
 		this.player.body.setVelocityY(80);
-	} //x and y respectively: +60, -90
+		this.player.anims.play('crounch', true);
+		this.player.body.setSize(120, 190, 0, 0);
+		this.player.body.setOffset(30, 20);
+	}
 	if (this.keys.q.isDown && this.cursors.right.isUp && this.cursors.left.isUp) {
 		this.player.anims.play('lP', 24);
 		if (hit != 1) {
@@ -110,6 +125,11 @@ update: function (time, delta)
 	}
 	if (this.keys.q.isUp) {
 		hit = 0;
+	}
+		if (this.cursors.down.isUp) {
+		
+		this.player.body.setSize(120, 300, 0, 0);
+		this.player.body.setOffset(30, 20);
 	}
 
 }
